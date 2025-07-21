bfd190307ff5d20fdfca036ef9783af42727328f
Floriannee <floriannepan@gmail.com>
2025-07-21 21:03:10 +0200

    Création de nouvelles routes pour le blog

--- src/app/pages/blog/blog.ts
+++ src/app/pages/blog/blog.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './blog.html',
   styleUrl: './blog.css'
 })
