af2960844f05e45b821634c250b549275fb96bdc
Floriannee <floriannepan@gmail.com>
2025-07-30 14:20:01 +0200

    Commit à jour

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ImageCarousel } from '../../image-carousel/image-carousel';
+import { ImageCarousel } from '../../components/image-carousel/image-carousel';
 
 @Component({
   selector: 'app-home',
